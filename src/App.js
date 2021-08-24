22862e1873411045d9418fc41ed1eda95b68e01e
Mehran Jafari <mehran.jafari70@gmail.com>
2021-08-24 22:02:26 +0430

    start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import Error404 from "./pages/error404";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+            <Route path="/" component={Dashboard} exact={true}/>
+            <Route path="/login" component={Login} exact={true} />
+            <Route path="*" component={Error404}/>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/error404/index.js
@@ -0,0 +1,8 @@
+const Error404 = () => {
+    return(
+        <h1>
+            Error 404 page
+        </h1>
+    )
+}
+export default Error404;
